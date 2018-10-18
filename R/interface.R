814f52598eeae754dd0b748e23ad53b2a6d2a72f
kcf-jackson <kcf.jackson@gmail.com>
2018-10-19 01:30:16 +1100

    Add interface to bridge between classes

---
+++ R/interface.R
@@ -0,0 +1,7 @@
+#' @include special_matrix.R
+NULL
+
+zero_matrix0 <- memo_zero_matrix
+Diagonal0 <- memo_Diagonal
+commutation_matrix0 <- memo_commutation_matrix
+elimination_matrix0 <- memo_elimination_matrix
