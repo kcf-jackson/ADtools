6f50fea353499a9f773d42d0db6702a2888111be
kcf-jackson <kcf.jackson@gmail.com>
2018-11-30 08:03:40 +1100

    Add matrix of ones

--- R/interface.R
+++ R/interface.R
@@ -2,6 +2,7 @@
 NULL
 
 zero_matrix0 <- memo_zero_matrix
+one_matrix0 <- memo_one_matrix
 Diagonal0 <- memo_Diagonal
 commutation_matrix0 <- memo_commutation_matrix
 elimination_matrix0 <- memo_elimination_matrix
--- R/special_matrix.R
+++ R/special_matrix.R
@@ -18,6 +18,12 @@ zero_matrix <- function(nr, nc) {
   Matrix::Matrix(data = 0, nrow = nr, ncol = nc)
 }
 
+one_matrix <- function(nr, nc, x = 1) {
+  dim0 <- c(nr, nc)
+  seq_n <- seq(min(dim0))
+  Matrix::sparseMatrix(seq_n, seq_n, x = x, dims = dim0)
+}
+
 memoize <- function(f) {
   table0 <- list()
   char_hash <- function(...) {
@@ -35,6 +41,7 @@ memoize <- function(f) {
 }
 
 memo_zero_matrix <- memoize(zero_matrix)
+memo_one_matrix <- memoize(one_matrix)
 memo_Diagonal <- memoize(Matrix::Diagonal)
 memo_commutation_matrix <- memoize(commutation_matrix)
 memo_elimination_matrix <- memoize(elimination_matrix)
