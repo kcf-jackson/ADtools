57fda6b896c1059a0f847c394c606270b7d78748
kcf-jackson <kcf.jackson@gmail.com>
2018-12-03 15:05:30 +1100

    Update documentation

--- R/finite_diff.R
+++ R/finite_diff.R
@@ -1,4 +1,8 @@
-# Finite differencing
+#' Finite differencing
+#' @param f A function of which the derivative is seeked.
+#' @param x The point at which the derivative is required.
+#' @param h The finite differencing parameter; the size of perturbation.
+#' @export
 finite_diff <- function(f, x, h = 1e-8) {
   perturbate <- function(v, h) {
     purrr::map(seq_along(v), function(i) { v[i] <- v[i] + h; v })
--- R/interface.R
+++ R/interface.R
@@ -1,8 +1,36 @@
 #' @include special_matrix.R
 NULL
 
+#' Matrix of zeroes
+#' @param nr integer; number of rows.
+#' @param nc integer; number of columns.
+#' @export
 zero_matrix0 <- memo_zero_matrix
+
+
+#' Matrix of ones
+#' @param nr integer; number of rows.
+#' @param nc integer; number of columns.
+#' @param x A scalar or a vector to be placed on the diagonal of the matrix.
+#' @export
 one_matrix0 <- memo_one_matrix
+
+
+#' Diagonal matrix
+#' @param n integer; the dimension of the square matrix.
+#' @param x A scalar or a vector to be placed on the diagonal of the matrix.
+#' @export
 Diagonal0 <- memo_Diagonal
+
+
+#' Commutation matrix
+#' @param r integer; row dimension.
+#' @param c integer; column dimension.
+#' @export
 commutation_matrix0 <- memo_commutation_matrix
+
+
+#' Elimination matrix
+#' @param n integer, row or column dimension.
+#' @export
 elimination_matrix0 <- memo_elimination_matrix
