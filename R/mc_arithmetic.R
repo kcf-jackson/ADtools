c37df4b8dda7a9a006033dec1ac5107bcbec6566
kcf-jackson <kcf.jackson@gmail.com>
2018-12-20 18:35:37 +1100

    Fix mismatch of dimension of x and dx

--- R/mc_arithmetic.R
+++ R/mc_arithmetic.R
@@ -11,21 +11,29 @@ NULL
 #' @param e2 "ANY" object.
 setMethod("+",
           signature(e1 = "dual", e2 = "ANY"),
-          function(e1, e2) { e1@x <- parent_of(e1) + e2; e1 })
+          function(e1, e2) {
+            e1@x <- parent_of(e1) + e2
+            if (is_matrix(e2) && (length(e2) != nrow(deriv_of(e1)))) {
+              e1@dx <- seq(length(e2)) %>%
+                purrr::map(~deriv_of(e1)) %>%
+                do.call(rbind, .)
+            }
+            e1
+          })
 
 #' Addition of 'dual'-class objects
 #' @param e1 "ANY" object.
 #' @param e2 A "dual" object.
 setMethod("+",
           signature(e1 = "ANY", e2 = "dual"),
-          function(e1, e2) { e2@x <- e1 + parent_of(e2); e2 })
+          function(e1, e2) { e2 + e1 })
 
 #' Subtraction of 'dual'-class objects
 #' @param e1 A "dual" object.
 #' @param e2 "ANY" object.
 setMethod("-",
           signature(e1 = "dual", e2 = "ANY"),
-          function(e1, e2) { e1@x <- parent_of(e1) - e2; e1})
+          function(e1, e2) { e1 + (-e2) })
 
 #' Subtraction of 'dual'-class objects
 #' @param e1 A "dual" object.
@@ -44,11 +52,7 @@ setMethod("-",
 #' @param e2 A "dual" object.
 setMethod("-",
           signature(e1 = "ANY", e2 = "dual"),
-          function(e1, e2) {
-            e2@x <- e1 - parent_of(e2)
-            e2@dx <- - deriv_of(e2)
-            e2
-          }
+          function(e1, e2) { e1 + (-e2) }
 )
 
 #' (Element-wise) Multiplication of 'dual'-class objects
@@ -61,9 +65,15 @@ setMethod("*",
               e1@x <- parent_of(e1) * e2
               e1@dx <- deriv_of(e1) * e2
             } else {
-              # matrix case
-              e1@x <- parent_of(e1) * e2
-              e1@dx <- deriv_of(e1) * as.numeric(e2)
+              if (is_scalar(e1@x)) {
+                # e1 is scalar-dual, e2 is matrix
+                e1@x <- parent_of(e1) * e2
+                e1@dx <- as.numeric(e2) %*% deriv_of(e1)
+              } else {
+                # e1 is matrix-dual, e2 is matrix
+                e1@x <- parent_of(e1) * e2
+                e1@dx <- deriv_of(e1) * as.numeric(e2)
+              }
             }
             e1
           }
@@ -79,9 +89,15 @@ setMethod("*",
               e2@x <- e1 * parent_of(e2)
               e2@dx <- e1 * deriv_of(e2)
             } else {
-              # matrix case
-              e2@x <- e1 * parent_of(e2)
-              e2@dx <- as.numeric(e1) * deriv_of(e2)
+              if (is_scalar(e2@x)) {
+                # e1 is matrix, e2 is dual-scalar
+                e2@x <- e1 * parent_of(e2)
+                e2@dx <- as.numeric(e1) %*% deriv_of(e2)
+              } else {
+                # e1 is matrix, e2 is dual-matrix
+                e2@x <- e1 * parent_of(e2)
+                e2@dx <- as.numeric(e1) * deriv_of(e2)
+              }
             }
             e2
           }
@@ -97,8 +113,13 @@ setMethod("/",
               e1@x <- parent_of(e1) / e2
               e1@dx <- deriv_of(e1) / e2
             } else {
-              e1@x <- parent_of(e1) / e2
-              e1@dx <- deriv_of(e1) / as.numeric(e2)
+              if (is_scalar(e1@x)) {
+                e1@x <- parent_of(e1) / e2
+                e1@dx <- (rep(1, length(e2)) %*% deriv_of(e1)) / as.numeric(e2)
+              } else {
+                e1@x <- parent_of(e1) / e2
+                e1@dx <- deriv_of(e1) / as.numeric(e2)
+              }
             }
             e1
           }
@@ -112,44 +133,67 @@ setMethod("/",
           function(e1, e2) {
             x <- e2@x
             dx <- e2@dx
-            for (j in 1:ncol(x)) {
-              for (i in 1:nrow(x)) {
-                deriv_rind <- i + (j-1) * nrow(x)
-                inv_a <- 1 / x[i, j, drop = F]
-                d_inv_a <- - as.numeric(inv_a)^2 * dx[deriv_rind, , drop = F]
-                k <- ifelse(is_scalar(e1), e1, e1[i, j])
-                e2@x[i, j] <- k * inv_a
-                e2@dx[deriv_rind, ] <- k * d_inv_a
+            if (is_scalar(x)) {
+              inv_a <- 1/ x
+              d_inv_a <- - inv_a^2 * dx
+              e2@x <- e1 * inv_a
+              e2@dx <- as.numeric(e1) %*% d_inv_a
+            } else {
+              for (j in 1:ncol(x)) {
+                for (i in 1:nrow(x)) {
+                  deriv_rind <- i + (j-1) * nrow(x)
+                  inv_a <- as.numeric(1 / x[i, j, drop = F])
+                  d_inv_a <- - inv_a^2 * dx[deriv_rind, , drop = F]
+                  k <- ifelse(is_scalar(e1), e1, e1[i, j])
+                  e2@x[i, j] <- k * inv_a
+                  e2@dx[deriv_rind, ] <- k * d_inv_a
+                }
               }
             }
             return(e2)
           }
 )
 
+get_param_dim <- function(x) {
+  param_dim <- param_of(x)
+  param_dim %>%
+    col_range_to_dim() %>%
+    setNames(names(param_dim)) %>%
+    as.list()
+}
+
 #' Matrix multiplication of 'dual'-class objects
 #' @param x A "dual" object.
 #' @param y "ANY" object.
 setMethod("%*%",
           signature(x = "dual", y = "ANY"),
-          function(x, y) { x %*% dual(y, ncol(x@dx), -1) })
+          function(x, y) {
+            x %*% dual(y, get_param_dim(x), -1)
+          })
 
 #' Matrix multiplication of 'dual'-class objects
 #' @param x "ANY" object.
 #' @param y A "dual" object.
 setMethod("%*%",
           signature(x = "ANY", y = "dual"),
-          function(x, y) { dual(x, ncol(y@dx), -1) %*% y })
+          function(x, y) {
+            dual(x, get_param_dim(y), -1) %*% y
+          })
 
 #' Kronecker multiplication of 'dual'-class objects
 #' @param X A "dual" object.
 #' @param Y "ANY" object.
 setMethod("kronecker",
           signature(X = "dual", Y = "ANY"),
-          function(X, Y) { X %x% dual(Y, ncol(X@dx), -1) })
+          function(X, Y) {
+            X %x% dual(Y, get_param_dim(X), -1)
+          })
 
 #' Kronecker multiplication of 'dual'-class objects
 #' @param X "ANY" object.
 #' @param Y A "dual" object.
 setMethod("kronecker",
           signature(X = "ANY", Y = "dual"),
-          function(X, Y) { dual(X, ncol(Y@dx), -1) %x% Y })
+          function(X, Y) {
+            dual(X, get_param_dim(Y), -1) %x% Y
+          })
