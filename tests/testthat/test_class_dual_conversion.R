c77db2fe7d882126fa69d7a51b0eed01e64a0020
kcf-jackson <kcf.jackson@gmail.com>
2019-01-04 21:03:51 +1100

    Add conversion helper functions

--- tests/testthat/test_class_dual_conversion.R
+++ tests/testthat/test_class_dual_conversion.R
@@ -1,3 +1,24 @@
+testthat::test_that("Test AD source-code overloading", {
+  f <- function(y, X, beta) { y - X %*% beta }
+  vary <- list(beta = c(5, 6))
+  fix <- list(X = matrix(1:4, 2, 2), y = c(2,3))
+  res_AD <- auto_diff(f, vary = vary, fix = fix)
+  res_FD <- finite_diff(f, vary = vary, fix = fix)
+  testthat::expect_lt(
+    max(relative_diff(get_deriv(res_AD), res_FD)), 1e-7
+  )
+
+  g <- function(X, Y) { X %*% Y }
+  X <- randn(2, 2)
+  Y <- randn(2, 2)
+  res_AD <- auto_diff(g, vary = list(X = X, Y = Y))
+  res_FD <- finite_diff(g, vary = list(X = X, Y = Y))
+  testthat::expect_lt(
+    max(relative_diff(get_deriv(res_AD), res_FD)), 1e-7
+  )
+})
+
+
 testthat::test_that("Single-layer destructuring", {
   list(x, y) %<-% list(1, 2)
   testthat::expect_equal(x, 1)
@@ -10,5 +31,5 @@ testthat::test_that("Single-layer destructuring", {
   testthat::expect_error(c(x, y) %<-% list(3))
   testthat::expect_error(c(x, y) %<-% c(3, 3))
   testthat::expect_error(list(x, y) %<-% c(3, 6))
-  testthat::expect_error(list(x, y, z) %<-% c(3, 6))
+  testthat::expect_error(list(x, y, z) %<-% list(3, 6))
 })
