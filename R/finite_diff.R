2def029a7097e54cc409afc5ae51299813aeb792
kcf-jackson <kcf.jackson@gmail.com>
2019-01-04 14:11:31 +1100

    Add new finite differencing implementation

--- R/finite_diff.R
+++ R/finite_diff.R
@@ -1,15 +1,93 @@
 #' Finite differencing
 #' @param f A function of which the derivative is seeked.
-#' @param x The point at which the derivative is required.
+#' @param vary A named list of variables; the variables to be varied.
+#' @param fix A named list of variables; the variables to be fixed.
 #' @param h The finite differencing parameter; the size of perturbation.
+#' @examples
+#' f <- function(y, X, beta) { y - X %*% beta }
+#' finite_difference(f,
+#'   vary = list(beta = c(5,6)),
+#'   fix = list(X = matrix(1:4, 2, 2), y = c(2,3))
+#' )
+#'
+#' g <- function(X, Y) { X %*% Y }
+#' finite_difference(g, vary = list(X = randn(2, 2), Y = randn(2, 2)))
+#'
 #' @export
-finite_diff <- function(f, x, h = 1e-8) {
+finite_difference <- function(f, vary, fix = NULL, h = 1e-8) {  # add fix
+  dim_len <- if_null_then(dim, length)
+  dim_ls <- purrr::map(vary, dim_len)
+
+  x <- list_to_vec(vary)
+  f_vec <- function(vec0) {
+    do.call(f, append(vec_to_list(vec0, dim_ls), fix))    # add append
+  }
+
+  vec_finite_diff(f_vec, x, h) %>%
+    magrittr::set_colnames(flatten_name(vary)) %>%
+    add_rownames()
+}
+
+
+#' Finite differencing (Core)
+#' @param f A function of which the derivative is seeked.
+#' @param x The point at which the derivative is seeked.
+#' @param h The finite differencing parameter; the size of perturbation.
+#' @examples
+#' \dontrun{
+#' f <- function(x) { x[1] + 2* x[2] }
+#' vec_finite_diff(f, c(2,3))
+#' }
+#' @keywords internal
+vec_finite_diff <- function(f, x, h = 1e-8) {
+  fx <- f(x)  # avoid duplicate evaluation
+  finite_deriv <- . %>% { as.numeric((f(.) - fx) / h) }
   perturbate <- function(v, h) {
     purrr::map(seq_along(v), function(i) { v[i] <- v[i] + h; v })
   }
-  diff_fun <- . %>% { as.numeric((f(.) - f(x)) / h) }
 
-  perturbate(x, h) %>%
-    purrr::map(diff_fun) %>%
-    do.call(cbind, .)
+  map_then_call(perturbate(x, h), finite_deriv, cbind)
+}
+
+
+# ===== Converting between list and vector =====
+list_to_vec <- function(list0) {
+  map_then_call(list0, as.numeric, c)
+}
+
+vec_to_list <- function(vec0, ls_dim) {
+  # extract elements according to ls_dim
+  vec0_index <- cumsum(purrr::map_dbl(ls_dim, prod))
+  ls_vec <- map_diff(c(0, vec0_index), ~vec0[seq(.x + 1, .y)])
+  # reshape elements according to ls_dim
+  res <- purrr::map2(ls_vec, ls_dim, ~structure(.x, dim = .y))
+  setNames(res, names(ls_dim))
+}
+
+flatten_name <- function(list0) {
+  paste("d", names(list_to_vec(list0)), sep = "_")
+}
+
+add_rownames <- function(x) {
+  rownames(x) <- paste("d_output", seq(nrow(x)), sep = "_")
+  x
+}
+
+# ===== High-order helper functions =====
+map_then_call <- function(x, f, g) {
+  x %>% purrr::map(f) %>% do.call(g, .)
+}
+
+# Apply f1, if null, then apply f2
+if_null_then <- function(f1, f2) {
+  function(x) {
+    f1x <- f1(x)
+    if (is.null(f1x)) return(f2(x))
+    return(f1x)
+  }
+}
+
+# Apply f to every two consecutive items in a vector
+map_diff <- function(vec0, f) {
+  purrr::map2(head(vec0, -1), tail(vec0, -1), f)
 }
--- tests/testthat/test_finite_difference.R
+++ tests/testthat/test_finite_difference.R
@@ -1,19 +1,17 @@
-testthat::context("Test finite differencing")
+context("Test finite differencing")
 
-
-approx_eq <- function(expr, output) {
-  res <- eval(quote(expr))
-  testthat::expect_lt(sum(abs(res - output)), 1e-6)
-}
-
-
-testthat::test_that("Test finite difference", {
-  f1 <- function(a) { 2 * a + c(1,2,3) }  # R^3 -> R^3
-  approx_eq(finite_diff(f1, rnorm(3)), 2 * diag(3))
-
-  f2 <- function(a) { a * c(5,4,3) }  # R^3 -> R^3
-  approx_eq(finite_diff(f2, rnorm(3)), diag(c(5,4,3)))
-
-  f3 <- function(a) { c(2 * a, a[1]) }  # R^3 -> R^4
-  approx_eq(finite_diff(f3, rnorm(3)), rbind(2 * diag(3), c(1,0,0)))
+testthat::test_that("Test finite difference main function", {
+  f <- function(X, y) { X %*% y }
+  set.seed(123)
+  for (i in 1:10) {
+    X <- randn(2, 2)
+    y <- rnorm(2)
+    expect_lt(
+      max(relative_diff(
+        finite_difference(f, list(X = X, y = y)),
+        cbind(t(y %x% diag(2)), X)
+      )),
+      1e-6
+    )
+  }
 })
