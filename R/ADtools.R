f79e24214d12fd00b97c818bbfb42cb075bd0087
kcf-jackson <kcf.jackson@gmail.com>
2019-02-24 02:04:32 +1100

    Update imports and namespace

--- R/ADtools.R
+++ R/ADtools.R
@@ -2,7 +2,8 @@
 "_PACKAGE"
 
 # Suppress R CMD check note
-#' @importFrom stats rnorm setNames dgamma integrate pgamma rgamma runif uniroot
+#' @importFrom stats rnorm setNames dgamma integrate pgamma rgamma runif uniroot dt pt
 #' @importFrom magrittr %>%
+#' @importFrom utils relist
 #' @import Matrix
 utils::globalVariables(c("Var1", "Var2", "."))
