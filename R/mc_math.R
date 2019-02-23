ce1be232458ff4273b6986a3eb17fb4047320fd3
kcf-jackson <kcf.jackson@gmail.com>
2019-02-24 02:04:07 +1100

    Add mathematical functions

--- R/mc_math.R
+++ R/mc_math.R
@@ -13,11 +13,54 @@ element_wise_derivative <- function(f, df) {
   }
 }
 
-d_sqrt <- function(x) { 0.5 / sqrt(x) }
+lambda <- pryr::f
 
 #' Element-wise square-root of a dual object
 #' @param x A "dual" object.
-setMethod("sqrt",
-          signature(x = "dual"),
-          element_wise_derivative(sqrt, d_sqrt)
+setMethod(
+  "sqrt",
+  signature(x = "dual"),
+  element_wise_derivative(sqrt, lambda(0.5 / sqrt(x)))
+)
+
+#' Element-wise sine of a dual object
+#' @param x A "dual" object.
+setMethod(
+  "sin",
+  signature(x = "dual"),
+  element_wise_derivative(sin, cos)
+)
+
+#' Element-wise cosine of a dual object
+#' @param x A "dual" object.
+setMethod(
+  "cos",
+  signature(x = "dual"),
+  element_wise_derivative(cos, lambda(-sin(x)))
+)
+
+#' Element-wise tangent of a dual object
+#' @param x A "dual" object.
+setMethod(
+  "tan",
+  signature(x = "dual"),
+  element_wise_derivative(tan, lambda(cos(x)^{
+    -2
+  }))
+)
+
+#' Element-wise exponential of a dual object
+#' @param x A "dual" object.
+setMethod(
+  "exp",
+  signature(x = "dual"),
+  element_wise_derivative(exp, exp)
+)
+
+#' Element-wise logarithm of a dual object
+#' @param x A "dual" object.
+setMethod(
+  "log",
+  signature(x = "dual"),
+  element_wise_derivative(log, lambda(1 / x))
 )
