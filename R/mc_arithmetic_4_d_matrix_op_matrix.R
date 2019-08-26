4223baa62eeb37c6ce6bf22033f058cb4a59ac13
kcf-jackson <kcf.jackson@gmail.com>
2019-08-27 03:29:13 +1000

    Optimise performance by removing for-loops

--- R/mc_arithmetic_4_d_matrix_op_matrix.R
+++ R/mc_arithmetic_4_d_matrix_op_matrix.R
@@ -13,11 +13,16 @@ d_matrix_multiply_matrix <- function(a, b) {
   assertthat::assert_that(identical(dim(pa), dim(pb)))
   da <- a@dx
   db <- b@dx
-  d_res <- da
-  for (i in 1:nrow(da)) {
-    d_res[i, ] <- multiply_fun(da[i, ], db[i, ], pa[i], pb[i])
-  }
-  d_res
+  mapreduce(
+    1:nrow(da),
+    ~multiply_fun(da[.x, ], db[.x, ], pa[.x], pb[.x]),
+    rbind2
+  )
+  # d_res <- da
+  # for (i in 1:nrow(da)) {
+  #   d_res[i, ] <- multiply_fun(da[i, ], db[i, ], pa[i], pb[i])
+  # }
+  # d_res
 }
 
 d_matrix_divide_matrix <- function(a, b) {
@@ -27,9 +32,14 @@ d_matrix_divide_matrix <- function(a, b) {
   pb <- b@x
   da <- a@dx
   db <- b@dx
-  d_res <- da
-  for (i in 1:nrow(da)) {
-    d_res[i, ] <- divide_fun(da[i, ], db[i, ], pa[i], pb[i])
-  }
-  d_res
+  mapreduce(
+    1:nrow(da),
+    ~divide_fun(da[.x, ], db[.x, ], pa[.x], pb[.x]),
+    rbind2
+  )
+  # d_res <- da
+  # for (i in 1:nrow(da)) {
+  #   d_res[i, ] <- divide_fun(da[i, ], db[i, ], pa[i], pb[i])
+  # }
+  # d_res
 }
