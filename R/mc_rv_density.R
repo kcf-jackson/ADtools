e8f7b83c31194d298e6fb69109da5e6ad2b89c88
kcf-jackson <kcf.jackson@gmail.com>
2020-01-11 22:23:21 +1100

    Add density function

---
+++ R/mc_rv_density.R
@@ -0,0 +1,43 @@
+#' The density of the multivariate t distribution
+#' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
+#' @param delta the vector of noncentrality parameters.
+#' @param sigma scale matrix.
+#' @param df degress of freedom.
+#' @param log logical; whether to return log density value.
+#' @export
+#' @examples
+#' sigma <- crossprod(randn(3, 3))
+#' data <- rmvt0(2, sigma = sigma, df = 2)
+#' dmvt0(data, sigma = sigma, df = 2)
+dmvt0 <- function(x, delta = 0, sigma, df = 1, log = TRUE) {
+  p <- nrow(sigma)
+  a <- log(gamma(0.5 * (df + p))) - log(gamma(df / 2))
+  inv_sigma <- solve(sigma)
+  b <- 0.5 * p * log(df) + 0.5 * p * log(pi)
+  b2 <- 0.5 * log(det(sigma))
+  b3 <- 0.5 * (df + p)
+
+  # datum_LL <- function(x) {
+  #   b4 <- log(1 + as.vector(t(x - delta) %*% inv_sigma %*% (x - delta)) / df)
+  #   logretval <- a - b - b2 - b3 * b4
+  #   if (log) logretval else exp(logretval)
+  # }
+  # mapreduce(1:nrow(x), ~datum_LL(x[.x, ]), rbind2)
+
+  # cx <- t(t(x) - delta)
+  delta_mat <- t(mapreduce(1:nrow(x), ~delta, cbind2))
+  cx <- x - delta_mat
+  b4 <- log(1 + (rowSums(cx %*% inv_sigma * cx) / df))
+  logretval <- a - b - b2 - b3 * b4
+  if (log) logretval else exp(logretval)
+}
+
+# dmvnorm0
+# dnorm0
+# dt0
+# dmvt0
+# dgamma0
+# dWishart0
+# dchisq0
+# dexp0
+#
---
+++ tests/testthat/test_mc_rv_density.R
@@ -0,0 +1,24 @@
+context("Test differentiation of density function")
+
+testthat::test_that("Test multivariate t distribution", {
+  set.seed(123)
+  p <- 10
+  df <- 4
+  delta <- rnorm(p)
+  sigma <- diag(runif(p, min = 0.5))
+  x <- matrix(rnorm(2*p, mean = 2), nrow = 2, ncol = p)
+
+  # Check function implementation
+  testthat::expect_equal(
+    mvtnorm::dmvt(x, delta = delta, sigma = sigma, df = df),
+    as.numeric(dmvt0(x, delta = delta, sigma = sigma, df = df))
+  )
+
+  # Auto-differentiation
+  f <- function(df, delta, sigma) {
+    dmvt0(x = x, df = df, delta = delta, sigma = sigma)
+  }
+  inputs <- list(list(df = df, delta = delta, sigma = sigma))
+  ctrl <- list(display = T, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
