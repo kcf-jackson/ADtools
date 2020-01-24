5ec35e79560cb72ab5241c20881774b4a340ab82
kcf-jackson <kcf.jackson@gmail.com>
2020-01-24 16:03:34 +1100

    Add density functions for various distributions

--- R/mc_rv_density.R
+++ R/mc_rv_density.R
@@ -1,7 +1,7 @@
 #' The density of the multivariate t distribution
 #' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
 #' @param delta the vector of noncentrality parameters.
-#' @param sigma scale matrix.
+#' @param sigma numeric matrix; scale matrix.
 #' @param df degress of freedom.
 #' @param log logical; whether to return log density value.
 #' @examples
@@ -17,27 +17,146 @@ dmvt0 <- function(x, delta, sigma, df = 1, log = TRUE) {
   b2 <- 0.5 * log(det(sigma))
   b3 <- 0.5 * (df + p)
 
-  # datum_LL <- function(x) {
-  #   b4 <- log(1 + as.vector(t(x - delta) %*% inv_sigma %*% (x - delta)) / df)
-  #   logretval <- a - b - b2 - b3 * b4
-  #   if (log) logretval else exp(logretval)
-  # }
-  # mapreduce(1:nrow(x), ~datum_LL(x[.x, ]), rbind2)
-
   # cx <- t(t(x) - delta)
-  delta_mat <- t(mapreduce(1:nrow(x), ~delta, cbind2))
-  cx <- x - delta_mat
+  # cx <- add_vector_to_matrix_row(-delta, x)
+  cx <- x + one_matrix0(NROW(x), 1) %*% t(-delta)
   b4 <- log(1 + (rowSums(cx %*% inv_sigma * cx) / df))
-  logretval <- a - b - b2 - b3 * b4
+  logretval <- (a - b - b2) - b3 * b4
   if (log) logretval else exp(logretval)
 }
 
-# dmvnorm0
-# dnorm0
-# dt0
-# dmvt0
-# dgamma0
-# dWishart0
-# dchisq0
-# dexp0
-#
+
+# Compute t(x) %*% A %*% x
+quadratic_form <- function(x, A) {
+  rowSums(x %*% A * x)
+}
+
+
+#' The density of the multivariate normal distribution
+#' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
+#' @param mean numeric vector; the mean vector.
+#' @param sigma numeric matrix; the covariance matrix.
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dmvnorm0 <- function(x, mean, sigma, log = FALSE) {
+  d <- length(mean)
+  cx <- x + one_matrix0(NROW(x), 1) %*% t(-mean)
+  logretval <- - 0.5 * quadratic_form(cx, solve(sigma)) -
+    (d / 2) * log(2 * pi) - 0.5 * log(det(sigma))
+  if (log) logretval else exp(logretval)
+}
+
+
+#' The density of the normal distribution
+#' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
+#' @param mean numeric vector; the mean vector.
+#' @param sd numeric vector; the sd vector.
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dnorm0 <- function(x, mean, sd, log = FALSE) {
+  cx <- x + one_matrix0(NROW(x), 1) %*% t(-mean)
+  logretval <- as.vector(-0.5 * (cx / sd)^2 - log(sd) - 0.5 * log(2 * pi))
+  if (log) logretval else exp(logretval)
+}
+
+
+#' The density of the student-t distribution
+#' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
+#' @param df degrees of freedom (> 0, maybe non-integer).
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dt0 <- function(x, df, log = FALSE) {
+  logretval <- log(gamma((df + 1) / 2)) - 0.5 * log(df * pi) -
+    log(gamma(df / 2)) - (df + 1) / 2 * log(1 + x^2 / df)
+  if (log) logretval else exp(logretval)
+}
+
+
+#' The density of the gamma distribution
+#' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
+#' @param shape positive number; the shape parameter.
+#' @param rate positive number; the rate parameter.
+#' @param scale positive number; the scale parameter.
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dgamma0 <- function(x, shape, rate, scale = 1 / rate, log = FALSE) {
+  if (!missing(rate) && !missing(scale)) {
+    if (abs(rate * scale - 1) < 1e-15)
+      warning("specify 'rate' or 'scale' but not both")
+    else
+      stop("specify 'rate' or 'scale' but not both")
+  }
+  k <- shape
+  theta <- scale
+  logretval <- (k-1) * log(x) - x / theta - log(gamma(k)) - k * log(theta)
+  if (log) logretval else exp(logretval)
+}
+
+
+#' The density of the chi-squared distribution
+#' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
+#' @param df degrees of freedom (> 0, maybe non-integer).
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dchisq0 <- function(x, df, log = FALSE) {
+  half_k <- df / 2
+  logretval <- (half_k - 1) * log(x) - x / 2 - half_k * log(2) - log(gamma(half_k))
+  if (log) logretval else exp(logretval)
+}
+
+
+#' The density of the exponential distribution
+#' @param x vector or matrix of quantiles. If x is a matrix, each row is taken to be a quantile.
+#' @param rate positive number; the rate parameter.
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dexp0 <- function(x, rate = 1, log = FALSE) {
+  retval <- rate * exp(-rate * x)
+  if (log) log(retval) else retval
+}
+
+
+#' The density of the Wishart distribution
+#' @param X numeric matrix.
+#' @param df degrees of freedom (> 0, maybe non-integer).
+#' @param Scale numeric matrix; the scale matrix.
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dWishart0 <- function(X, df, Scale, log = FALSE) {
+  p <- nrow(X)
+  n <- df
+  # assertthat::assert_that(n > p - 1)
+  V <- Scale
+
+  logretval <- (n - p - 1) / 2 * log(det(X)) - tr(solve(V, X)) / 2 -
+    (n * p / 2) * log(2) - n / 2 * log(det(V)) - lmvgamma(p, n / 2)
+
+  if (log) logretval else exp(logretval)
+}
+
+lmvgamma <- function(p, a) {
+  if (p == 1) {
+    log(gamma(a))
+  } else {
+    0.5 * (p - 1) * log(pi) + lmvgamma(p - 1, a) + log(gamma(a + 0.5 * (1 - p)))
+  }
+}
+
+
+#' The density of the inverse Wishart distribution
+#' @param X numeric matrix.
+#' @param df degrees of freedom (> 0, maybe non-integer).
+#' @param Scale numeric matrix; the scale matrix.
+#' @param log logical; if TRUE, returns the log value.
+#' @export
+dinvWishart0 <- function(X, df, Scale, log = FALSE) {
+  p <- nrow(X)
+  v <- df
+  # assertthat::assert_that(v > p - 1)
+  Phi <- Scale
+
+  logretval <- v / 2 * log(det(Phi)) - (v + p + 1) / 2 * log(det(X)) -
+    0.5 * tr(Phi %*% solve(X)) - (v * p / 2) * log(2) - lmvgamma(p, v / 2)
+
+  if (log) logretval else exp(logretval)
+}
--- tests/testthat/test_mc_rv_density.R
+++ tests/testthat/test_mc_rv_density.R
@@ -1,6 +1,6 @@
 context("Test differentiation of density function")
 
-testthat::test_that("Test multivariate t distribution", {
+testthat::test_that("Test multivariate t density", {
   set.seed(123)
   p <- 10
   df <- 4
@@ -11,7 +11,7 @@ testthat::test_that("Test multivariate t distribution", {
   # Check function implementation
   testthat::expect_equal(
     mvtnorm::dmvt(x, delta = delta, sigma = sigma, df = df),
-    as.numeric(dmvt0(x, delta = delta, sigma = sigma, df = df))
+    dmvt0(x, delta = delta, sigma = sigma, df = df)
   )
 
   # Auto-differentiation
@@ -20,5 +20,174 @@ testthat::test_that("Test multivariate t distribution", {
   }
   inputs <- list(list(df = df, delta = delta, sigma = sigma))
   ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test multivariate Normal density", {
+  set.seed(123)
+  p <- 5
+  mean <- rnorm(p)
+  sigma <- diag(runif(p, min = 0.5))
+  n <- 20
+  x <- matrix(rnorm(n*p, mean = 2), nrow = n, ncol = p)
+
+  # Check function implementation
+  testthat::expect_equal(
+    mvtnorm::dmvnorm(x, mean = mean, sigma = sigma),
+    dmvnorm0(x, mean = mean, sigma = sigma)
+  )
+
+  # Auto-differentiation
+  f <- function(mean, sigma) {
+    dmvnorm0(x = x, mean = mean, sigma = sigma)
+  }
+  inputs <- list(list(mean = mean, sigma = sigma))
+  ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test Normal density", {
+  set.seed(123)
+  mean <- 1
+  sd <- 2
+  n <- 20
+  x <- rnorm(n, mean = mean, sd = sd)
+
+  # Check function implementation
+  testthat::expect_equal(
+    dnorm(x, mean = mean, sd = sd),
+    dnorm0(x, mean = mean, sd = sd)
+  )
+
+  # Auto-differentiation
+  f <- function(mean, sd) {
+    dnorm0(x = x, mean = mean, sd = sd)
+  }
+  inputs <- list(list(mean = mean, sd = sd))
+  ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test student-t density", {
+  set.seed(123)
+  df <- 5
+  n <- 20
+  x <- rt(n, df = df)
+
+  # Check function implementation
+  testthat::expect_equal(dt(x, df = df), dt0(x, df = df))
+
+  # Auto-differentiation
+  f <- function(df) dt0(x = x, df = df, log = F)
+  inputs <- list(list(df = df))
+  ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test Gamma density", {
+  set.seed(123)
+  shape <- 4
+  rate <- 9
+  n <- 20
+  x <- rgamma(n, shape = shape, rate = rate)
+
+  # Check function implementation
+  testthat::expect_equal(
+    dgamma(x, shape = shape, rate = rate),
+    dgamma0(x, shape = shape, rate = rate)
+  )
+
+  # Auto-differentiation
+  f <- function(shape, rate) {
+    dgamma0(x = x, shape = shape, rate = rate)
+  }
+  inputs <- list(list(shape = shape, rate = rate))
+  ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test Chi-squared density", {
+  set.seed(123)
+  df <- 5
+  n <- 20
+  x <- rchisq(n, df = df)
+
+  # Check function implementation
+  testthat::expect_equal(dchisq(x, df = df), dchisq0(x, df = df))
+
+  # Auto-differentiation
+  f <- function(df) dchisq0(x = x, df = df)
+  inputs <- list(list(df = df))
+  ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test Exponential density", {
+  set.seed(123)
+  rate <- 5
+  n <- 20
+  x <- rexp(n, rate = rate)
+
+  # Check function implementation
+  testthat::expect_equal(
+    dexp(x, rate = rate), dexp0(x, rate = rate)
+  )
+
+  # Auto-differentiation
+  f <- function(rate) dexp0(x = x, rate = rate)
+  inputs <- list(list(rate = rate))
+  ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test Wishart density", {
+  set.seed(123)
+  df <- 12
+  Sigma <- crossprod(randn(4, 4))
+  n <- 20
+  x <- rWishart(n, df = df, Sigma = Sigma)
+  y <- purrr::map(seq(dim(x)[3]), ~x[,,.x])
+  # Check function implementation
+  testthat::expect_equal(
+    purrr::map_dbl(y, ~MCMCpack::dwish(.x, v = df, S = Sigma)),
+    purrr::map_dbl(y, ~dWishart0(.x, df = df, Scale = Sigma))
+  )
+
+  # Auto-differentiation
+  f <- function(df, Sigma) {
+    dWishart0(y[[1]], df = df, Scale = Sigma)
+  }
+  inputs <- list(list(df = df, Sigma = Sigma))
+  ctrl <- list(display = F, err_fun = abs_err, epsilon = 1e-6)
+  test_fs(list(f), inputs, ctrl)
+})
+
+
+testthat::test_that("Test inverse Wishart density", {
+  set.seed(123)
+  df <- 12
+  Sigma <- crossprod(randn(4, 4))
+  n <- 20
+  x <- purrr::map(1:n, ~MCMCpack::riwish(v = df, S = Sigma))
+
+  # Check function implementation
+  testthat::expect_equal(
+    purrr::map_dbl(x, ~MCMCpack::diwish(.x, v = df, S = Sigma)),
+    purrr::map_dbl(x, ~dinvWishart0(.x, df = df, Scale = Sigma))
+  )
+
+  # Auto-differentiation
+  f <- function(df, Sigma) {
+    dinvWishart0(x[[1]], df = df, Scale = Sigma)
+  }
+  inputs <- list(list(df = df, Sigma = Sigma))
+  ctrl <- list(display = F, err_fun = rel_err, epsilon = 1e-5)
   test_fs(list(f), inputs, ctrl)
 })
